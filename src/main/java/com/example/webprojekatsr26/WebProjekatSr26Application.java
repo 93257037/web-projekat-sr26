24523acb76aa755b9f4c2e0b3c61e5cec925c5ba
krist <krist@LAPTOP-NBTNJTVJ>
2023-08-25 19:57:43 +0200

    Pocetak

---
+++ src/main/java/com/example/webprojekatsr26/WebProjekatSr26Application.java
@@ -0,0 +1,13 @@
+package com.example.webprojekatsr26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProjekatSr26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebProjekatSr26Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webprojekatsr26/WebProjekatSr26ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webprojekatsr26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProjekatSr26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
